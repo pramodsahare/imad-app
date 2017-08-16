a836777e618415e500e9cbb73b2acb53c65cd64c
pramodsahare <30650834+pramodsahare@users.noreply.github.com>
2017-08-16 11:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var ArticleOne ={
+    title: 'Article One | Pramod Sahare',
+    heading: 'Article One',
+    date: 'Aug 15, 2017',
+    content:` <p> his is content for my first page. This is content for my first page. This is content for my first page
+                This is content for my first page. This is content for my first page. This is content for my first page
+                This is content for my first page. This is content for my first page. This is content for my first page
+              </p> 
+     <p>        This is content for my first page. This is content for my first page. This is content for my first page.
+                This is content for my first page. This is content for my first page. This is content for my first page.
+                This is content for my first page. This is content for my first page. This is content for my first page.
+    </p>
+    <p>         This is content for my first page. This is content for my first page. This is content for my first page.
+                This is content for my first page. This is content for my first page. This is content for my first page.
+                This is content for my first page. This is content for my first page. This is content for my first page.
+    </p> `
+   
+    
+};
+
+var htmlTemplate= `
+<html>
+    <head>
+        <title>${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        
+         <div class="container">
+        <div>
+            <a href="/">Home </a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+
+
+
+`;
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
